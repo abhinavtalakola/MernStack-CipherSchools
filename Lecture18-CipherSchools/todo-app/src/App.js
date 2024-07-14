47458921cb0e3c54b6b7e96f60112a5bb8d2f22a
abhinavtalakola <150319702+abhinavtalakola@users.noreply.github.com>
2024-07-14 12:08:43 +0530

    Add files via upload

---
+++ Lecture18-CipherSchools/todo-app/src/App.js
@@ -0,0 +1,12 @@
+import {Component} from "react";
+import ToDoScreen from "./screens/ToDoScreen";
+
+class App extends Component{
+    render(){
+        return (
+            <ToDoScreen />
+        );
+    }
+}
+
+export default App;
