5f2bd51b233f22163b763adbc16a0e06662ed513
abhinavtalakola <150319702+abhinavtalakola@users.noreply.github.com>
2024-07-21 16:55:56 +0530

    Add files via upload

---
+++ Lecture24-CipherSchools/src/App.js
@@ -0,0 +1,17 @@
+// import {Component} from "react";
+import ToDoScreen from "./screens/ToDoScreen";
+
+/*
+class App extends Component{
+    render(){
+        return (
+            <ToDoScreen />
+        );
+    }
+}
+*/
+const App=()=>{
+    return <ToDoScreen/>;
+};
+
+export default App;
---
+++ Lecture24-CipherSchools/src/components/Task.js
@@ -0,0 +1,23 @@
+import { formatDate } from "../utils/DateUtil";
+
+const Task=({task:{title,description,CreatedDate}})=>{
+    return(
+        <div className="card">
+            <div className="content">
+                <div className="header">{title}</div>
+                <div className="meta">{formatDate(CreatedDate)}</div>
+                <div className="description">
+                    {description}
+                </div>
+            </div>
+            <div className="extra content">
+                <div className="ui two buttons">
+                    <div className="ui basic green button">Edit</div>
+                    <div className="ui basic red button">Delete</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Task;
