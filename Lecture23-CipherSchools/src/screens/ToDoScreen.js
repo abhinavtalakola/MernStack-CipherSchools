19664d8416f7fed675f93b3b72e42e420c43d7ff
abhinavtalakola <150319702+abhinavtalakola@users.noreply.github.com>
2024-07-21 16:43:37 +0530

    Add files via upload

---
+++ Lecture23-CipherSchools/src/screens/ToDoScreen.js
@@ -0,0 +1,48 @@
+// import {Component} from "react";
+
+import {useState} from "react";
+import Task from "../components/Task";
+import AddTask from "../components/Addtask";
+
+const ToDoScreen = () => {
+    const [taskList, setTaskList] = useState([]);
+
+    let addNewTask=(task)=>{
+        setTaskList([...taskList,{...task,CreatedDate:new Date()}]);
+    };
+
+    return (
+        <>
+        <div className="screen">
+            <h1 className="ui heading center">To Do List</h1>
+            <div>
+                <button
+                    onClick={(e) => {
+                        setTaskList([
+                            ...taskList,
+                            {
+                                title: "Gym",
+                                description: "Going to gym is good for health",
+                                CreatedDate:new Date(),
+                            },
+                        ]);
+                    }}
+                    className="ui secondary button"
+                >
+                    Add Task
+                </button>
+                <section>
+                <div class="ui cards">
+                    {taskList.map((task,index) => (
+                        <Task task={task} key={index}/>
+                    ))}
+                </div>
+                </section>
+            </div>
+            <AddTask/>
+        </div>
+        </>
+    );
+}
+
+export default ToDoScreen;
