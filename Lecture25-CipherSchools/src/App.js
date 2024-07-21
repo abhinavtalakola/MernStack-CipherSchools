7c484bf254318691adcf534a23b9e8c8d81ae056
abhinavtalakola <150319702+abhinavtalakola@users.noreply.github.com>
2024-07-21 19:38:00 +0530

    Add files via upload

---
+++ Lecture25-CipherSchools/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import AddTask from "./components/Addtask";
+import ToDoScreen from "./screens/ToDoScreen";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ToDoScreen/>,
+    },
+    {
+        path:"/add-task",
+        element:<AddTask/>,
+    }
+])
+
+const App=()=>{
+    useState [tasks,setTasks]=useState([]);
+    return <RouterProvider router={router}/>;
+};
+
+export default App;
